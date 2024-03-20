58cfafc5a8467a9eb621e64aaa658d59106cb4e0
Enmanuel <14-05775@usb.ve>
2024-03-20 13:14:38 -0400

    update branch

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_Do2wdmfm.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_DK4S7O6t.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_ZVDKbqHJ.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_mPxiA_r4.mjs');
-
-export { page };
