58b4d3434bd6b0e05a7420b3c200d3f0c96b0c37
Enmanuel <14-05775@usb.ve>
2024-03-20 12:34:23 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import auth from "auth-astro";
-
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), auth()],
   output: "server",
-  adapter: vercel()
+  adapter: vercelServerless()
 });
--- src/cv.d.ts
+++ src/cv.d.ts
@@ -49,7 +49,6 @@ export interface CV {
     endDate: DateStr | null
     summary:  string[]
     highlights: Highlight
-    github?:string
   }
   
   type DateStr = `${string}-${string}-${string}`
