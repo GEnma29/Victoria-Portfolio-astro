d2165fb3d2914e6c77cc6d84b1ac895c37764e91
Enmanuel <14-05775@usb.ve>
2024-03-20 12:57:55 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
-import db from "@astrojs/db";
-import auth from "auth-astro";
-import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [db(), auth()],
-  output: "server",
-  adapter: vercelServerless(),
-});
+export default defineConfig({});
